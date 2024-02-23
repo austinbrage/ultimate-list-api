b69654978faabc600a2cf5dd71a6a3f419a56063
agusb19 <agustinbrage19@gmail.com>
2024-02-23 11:01:02 -0300

    feat: add interface for users model
    (copied code): borrowed from personal-blog-api code

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod'
 import { userSchema, authSchema } from '../schemas/users'
+import { type RowDataPacket } from 'mysql2'
 
 export type UserType = {
     id: z.infer<typeof userSchema.id>
@@ -17,4 +18,22 @@ export type UserType = {
     authEmail: z.infer<typeof userSchema.authEmail> 
     authData: z.infer<typeof userSchema.authData> 
     fullData: z.infer<typeof userSchema.fullData>
+}
+
+export interface IUser {
+    getAll({ id }: UserType['id']): Promise<RowDataPacket[]>
+    getName({ name }: UserType['name']): Promise<RowDataPacket[]>
+    getEmail({ email }: UserType['email']): Promise<RowDataPacket[]>
+    getNickname({ nickname }: UserType['nickname']): Promise<RowDataPacket[]>
+    getId({ api_key }: UserType['apiKey']): Promise<RowDataPacket[]>
+    getByEmail({ email }: UserType['email']): Promise<RowDataPacket[]>
+    getIdPassword({ name }: UserType['name']): Promise<RowDataPacket[]>
+    getByExternalID({ auth_provider, external_id }: UserType['authData']): Promise<RowDataPacket[]>
+    changeName({ id, name }: UserType['idName']): Promise<RowDataPacket[]>
+    changeEmail({ id, email }: UserType['idEmail']): Promise<RowDataPacket[]>
+    changeNickname({ id, nickname }: UserType['idNickname']): Promise<RowDataPacket[]>
+    changePassword({ id, password }: UserType['idPassword']): Promise<RowDataPacket[]>
+    changeExternalID({ auth_provider, external_id, email }: UserType['authEmail']): Promise<RowDataPacket[]>
+    addNew({ name, password, email, nickname, auth_provider, external_id }: UserType['fullData']): Promise<number>
+    remove({ id }: UserType['id']): Promise<RowDataPacket[]>
 }
