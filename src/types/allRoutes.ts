15b2f6f45b41298d20506782501d04f3fa7f7dac
agusb19 <agustinbrage19@gmail.com>
2024-02-25 17:37:52 -0300

    feat: create app, and resources enum routes for each api route

---
+++ src/types/allRoutes.ts
@@ -0,0 +1,38 @@
+export enum APP {
+    VERSION_1 = '/ultimate-list'
+}
+
+export enum RESOURCES {
+    PING = '/ping',
+    USER = '/user',
+    CONCEPT = '/concept',
+    KNOWLEDGE = '/knowledge'
+}
+
+export enum USER {
+    DATA = '/data',
+    NAME = '/name',
+    EMAIL = '/email',
+    NICKNAME = '/nickname',
+    PASSWORD = '/password',
+    REGISTER = '/register',
+    LOGIN = '/login',
+    OAUTH = '/oauth',
+    KEY = '/key',
+}
+
+export enum KNOWLEDGE {
+    DATA = '/data',
+    NAME = '/name',
+    TYPE = '/type',
+    PRIORITY = '/priority',
+    DESCRIPTION = '/description'
+}
+
+export enum CONCEPT {
+    DATA = '/data',
+    NAME = '/name',
+    TYPE = '/type',
+    PRIORITY = '/priority',
+    DESCRIPTION = '/description'
+}
