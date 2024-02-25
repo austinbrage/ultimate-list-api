7fb2f807dba9bef7fecbd374f5da4fb078199484
agusb19 <agustinbrage19@gmail.com>
2024-02-25 18:59:52 -0300

    feat: add researchs schema, types and validations
    (add types for research model and controller)

---
+++ src/types/researchs.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod'
+import { researchSchema } from "../schemas/researchs"
+import { type RowDataPacket } from 'mysql2'
+import { type AsyncFunction } from '../services/errorHandler'
+
+export type ResearchTypes = {
+    id: z.infer<typeof researchSchema.id>
+    userId: z.infer<typeof researchSchema.userId>
+    idType: z.infer<typeof researchSchema.idType>
+    idName: z.infer<typeof researchSchema.idName>
+    idPriority: z.infer<typeof researchSchema.idPriority>
+    userIdName: z.infer<typeof researchSchema.userIdName>
+    idDescription: z.infer<typeof researchSchema.idDescription>
+    fullData: z.infer<typeof researchSchema.fullData>
+}
+
+export interface IResearch {
+    getAll({ user_id }: ResearchTypes['userId']): Promise<RowDataPacket[]>
+    getName({ user_id, name }: ResearchTypes['userIdName']): Promise<RowDataPacket[]>
+    changePriority({ id, priority }: ResearchTypes['idPriority']): Promise<RowDataPacket[]>
+    changeType({ id, type }: ResearchTypes['idType']): Promise<RowDataPacket[]>
+    changeName({ id, name }: ResearchTypes['idName']): Promise<RowDataPacket[]>
+    changeDescription({ id, description }: ResearchTypes['idDescription']): Promise<RowDataPacket[]>
+    addNew({ user_id, type, name, priority, description }: ResearchTypes['fullData']): Promise<RowDataPacket[]>
+    remove({ id }: ResearchTypes['id']): Promise<RowDataPacket[]>
+}
+
+export interface ResearchController {
+    getAll: AsyncFunction    
+    changePriority: AsyncFunction
+    changeType: AsyncFunction
+    changeName: AsyncFunction
+    changeDescription: AsyncFunction
+    addNew: AsyncFunction
+    remove: AsyncFunction
+}
---
+++ src/validations/Researchs.ts
@@ -0,0 +1,29 @@
+import { researchSchema } from '../schemas/researchs'
+import { type ResearchTypes } from '../types/researchs'
+import { type SafeParseReturnType } from 'zod'
+
+export interface IResearchValidation {
+    id(data: unknown): SafeParseReturnType<unknown, ResearchTypes['id']> 
+    userId(data: unknown): SafeParseReturnType<unknown, ResearchTypes['userId']> 
+    idType(data: unknown): SafeParseReturnType<unknown, ResearchTypes['idType']> 
+    idName(data: unknown): SafeParseReturnType<unknown, ResearchTypes['idName']> 
+    fullData(data: unknown): SafeParseReturnType<unknown, ResearchTypes['fullData']> 
+    idPriority(data: unknown): SafeParseReturnType<unknown, ResearchTypes['idPriority']> 
+    idDescription(data: unknown): SafeParseReturnType<unknown, ResearchTypes['idDescription']> 
+}
+
+export class ResearchValidation implements IResearchValidation {
+    private researchSchema: typeof researchSchema
+
+    constructor() {
+        this.researchSchema = researchSchema
+    }
+
+    id = (data: unknown) => this.researchSchema.id.safeParse(data)
+    userId = (data: unknown) => this.researchSchema.userId.safeParse(data)
+    idType = (data: unknown) => this.researchSchema.idType.safeParse(data)
+    idName = (data: unknown) => this.researchSchema.idName.safeParse(data)
+    fullData = (data: unknown) => this.researchSchema.fullData.safeParse(data)
+    idPriority = (data: unknown) => this.researchSchema.idPriority.safeParse(data)
+    idDescription = (data: unknown) => this.researchSchema.idDescription.safeParse(data)
+}
