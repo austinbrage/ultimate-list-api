f1bc4cd47beaeaf995bbcc4a367c41d3beb8c3be
agusb19 <agustinbrage19@gmail.com>
2024-02-28 23:45:46 -0300

    feat: add all apiRutes to routes folder
    (to implement brage architecture later on)

---
+++ src/routes/apiRoutes.ts
@@ -0,0 +1,64 @@
+export enum APP {
+    VERSION_1 = '/ultimate-list'
+}
+
+export enum RESOURCES {
+    PING = '/ping',
+    USER = '/user',
+    ANSWER = '/answer',
+    CONCEPT = '/concept',
+    RESEARCH = '/research',
+    QUESTION = '/question',
+    KNOWLEDGE = '/knowledge'
+}
+
+export enum USER {
+    DATA = '/data',
+    NAME = '/name',
+    EMAIL = '/email',
+    NICKNAME = '/nickname',
+    PASSWORD = '/password',
+    REGISTER = '/register',
+    LOGIN = '/login',
+    OAUTH = '/oauth',
+    KEY = '/key',
+}
+
+export enum KNOWLEDGE {
+    DATA = '/data',
+    NAME = '/name',
+    TYPE = '/type',
+    PRIORITY = '/priority',
+    DESCRIPTION = '/description'
+}
+
+export enum CONCEPT {
+    DATA = '/data',
+    NAME = '/name',
+    TYPE = '/type',
+    PRIORITY = '/priority',
+    DESCRIPTION = '/description'
+}
+
+export enum RESEARCH {
+    DATA = '/data',
+    NAME = '/name',
+    TYPE = '/type',
+    PRIORITY = '/priority',
+    DESCRIPTION = '/description'
+}
+
+export enum QUESTION {
+    DATA = '/data',
+    NAME = '/name',
+    PRIORITY = '/priority',
+    DESCRIPTION = '/description'
+}
+
+export enum ANSWER {
+    DATA = '/data',
+    NAME = '/name',
+    TYPE = '/type',
+    PRIORITY = '/priority',
+    DESCRIPTION = '/description'
+}
