9c9313d45588802d4b4530f7d4143bc6c0ebe6e9
agusb19 <agustinbrage19@gmail.com>
2024-02-29 11:07:20 -0300

    feat: copy all concepts endpoint code to routes/concepts folder

---
+++ src/routes/concepts/schemas/concepts.ts
@@ -0,0 +1,63 @@
+import { z } from 'zod'
+
+export const knowledgeConceptTableSchema = z.object({
+    id: z.number({
+        required_error: 'Knowledge-Concept id is required',
+        invalid_type_error: 'Knowledge-Concept id must be a number'
+    }),
+    knowledge_id: z.number({
+        required_error: 'Knowledge id is required',
+        invalid_type_error: 'Knowledge id must be a number'
+    }),
+    priority: z.number({
+        required_error: 'Knowledge-Concept priority is required',
+        invalid_type_error: 'Knowledge-Concept priority must be a number'
+    }),
+    type: z.string({
+        required_error: 'Knowledge-Concept type is required',
+        invalid_type_error: 'Knowledge-Concept type must be a string'
+    }),
+    name: z.string({
+        required_error: 'Knowledge-Concept name is required',
+        invalid_type_error: 'Knowledge-Concept name must be a string'
+    }),
+    description: z.string({
+        required_error: 'Knowledge-Concept description is required',
+        invalid_type_error: 'Knowledge-Concept description must be a string'
+    }),
+})
+
+const id = knowledgeConceptTableSchema.pick({ id: true })
+const knowledgeId = knowledgeConceptTableSchema.pick({ knowledge_id: true })
+const idType = knowledgeConceptTableSchema.pick({ id: true, type: true })
+const idName = knowledgeConceptTableSchema.pick({ knowledge_id: true, id: true, name: true })
+const idPriority = knowledgeConceptTableSchema.pick({ id: true, priority: true })
+const knowledgeIdName = knowledgeConceptTableSchema.pick({ knowledge_id: true, name: true })
+const idDescription = knowledgeConceptTableSchema.pick({ id: true, description: true })
+const fullData = knowledgeConceptTableSchema.omit({ id: true })
+const data = knowledgeConceptTableSchema.omit({ id: true, priority: true })
+
+export const conceptSchema = {
+    id,
+    knowledgeId,
+    idType,
+    idName,
+    idPriority,
+    knowledgeIdName,
+    idDescription,
+    fullData,
+    data
+}
+
+const knowledgeConceptQuerySchema = z.object({
+    knowledge_id: z.string({
+        required_error: 'Knowledge id is required',
+        invalid_type_error: 'Knowledge id must be a string'
+    })
+}) 
+
+const knowledgeIdQuery = knowledgeConceptQuerySchema.pick({ knowledge_id: true })
+
+export const conceptQuerySchema = {
+    knowledgeIdQuery
+}
---
+++ src/routes/concepts/tests/integration/concepts.test.ts
@@ -0,0 +1,190 @@
+import request from 'supertest'
+import { USER, KNOWLEDGE, CONCEPT } from '../../../apiRoutes'
+import { userMock, userRoutes } from "../../../users/tests/fixtures/userData"
+import { knowledgeMock, knowledgeRoutes } from "../../../knowledge/tests/fixtures/knowledgeData"
+import { conceptMock, conceptRoutes } from '../fixtures/conceptData'
+import { app, server, userPool, knowledgePool } from '../../../../server'
+
+let token: string
+let conceptId: number
+let knowledgeId: number
+
+describe('Setup: Get access token from user sign up and in', () => { 
+    
+    test('should SIGN-UP new user', async () => {
+        await request(app)
+            .post(userRoutes(USER.REGISTER))
+            .send(userMock.signUp)
+            .expect(201)
+    })
+
+    test('should LOGIN to users account', async () => {
+        const response = await request(app)
+            .post(userRoutes(USER.LOGIN))
+            .send(userMock.rightData)
+            .expect(200)
+        token = response.body.result.token
+    })  
+
+})
+
+describe('Setup: Post new knowledge card and get the id', () => {
+
+    test('should POST new knowledge card', async () => {
+        await request(app)
+            .post(knowledgeRoutes(KNOWLEDGE.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .send(knowledgeMock.newData1)
+            .expect(201)
+    })
+
+    test('should READ and GET ID from new knowledge card', async () => {
+        const response = await request(app)
+            .get(knowledgeRoutes(KNOWLEDGE.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+        knowledgeId = response.body?.result?.data[0]?.id
+    })
+    
+})
+
+describe('Test create and read new knowledge concept', () => {
+
+    test('should POST new knowledge concept', async () => {
+        await request(app)
+            .post(conceptRoutes(CONCEPT.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .send(conceptMock.newData1(knowledgeId))
+            .expect(201)
+    })
+
+    test('should READ and GET ID from new knowledge concept', async () => {
+        const response = await request(app)
+            .get(conceptRoutes(CONCEPT.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .query(conceptMock.knowledgeId(knowledgeId))
+            .expect(200)
+        conceptId = response.body?.result?.data[0]?.id
+
+        expect(response.body.result.data[0]).toEqual(
+            expect.objectContaining(conceptMock.newData1(knowledgeId))
+        )         
+    })
+
+})
+
+describe('Test update new knowledge concept', () => {
+
+    test('should PATCH NAME of new concept', async () => {
+        await request(app)
+            .patch(conceptRoutes(CONCEPT.NAME))
+            .set('Authorization', `Bearer ${token}`)
+            .send(conceptMock.name(conceptId, knowledgeId))
+            .expect(200)
+    })
+
+    test('should PATCH TYPE of new concept', async () => {
+        await request(app)
+            .patch(conceptRoutes(CONCEPT.TYPE))
+            .set('Authorization', `Bearer ${token}`)
+            .send(conceptMock.type(conceptId))
+            .expect(200)
+    })
+
+    test('should PATCH PRIORITY of new concept', async () => {
+        await request(app)
+            .patch(conceptRoutes(CONCEPT.PRIORITY))
+            .set('Authorization', `Bearer ${token}`)
+            .send(conceptMock.priority(conceptId))
+            .expect(200)
+    })
+
+    test('should PATCH DESCRIPTION of new concept', async () => {
+        await request(app)
+            .patch(conceptRoutes(CONCEPT.DESCRIPTION))
+            .set('Authorization', `Bearer ${token}`)
+            .send(conceptMock.description(conceptId))
+            .expect(200)
+    })
+
+    test('should READ and GET ID from new knowledge concept', async () => {
+        const response = await request(app)
+            .get(conceptRoutes(CONCEPT.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .query(conceptMock.knowledgeId(knowledgeId))
+            .expect(200)
+
+        expect(response.body.result.data[0]).toEqual(
+            expect.objectContaining(conceptMock.changedData(knowledgeId))
+        )         
+    })
+
+})
+
+
+describe('Test priority value of new knowledge concept', () => {
+
+    test('should POST new knowledge concept', async () => {
+        await request(app)
+            .post(conceptRoutes(CONCEPT.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .send(conceptMock.newData2(knowledgeId))
+            .expect(201)
+    })
+
+    test('should READ and GET ID from new knowledge concept', async () => {
+        const response = await request(app)
+            .get(conceptRoutes(CONCEPT.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .query(conceptMock.knowledgeId(knowledgeId))
+            .expect(200)
+        conceptId = response.body?.result?.data[0]?.id
+
+        expect(response.body.result.data[0]).toEqual(
+            expect.objectContaining({
+                ...conceptMock.newData2(knowledgeId),
+                priority: conceptMock.priority(conceptId).priority + 1
+            })
+        )         
+    })
+
+})
+
+describe('Test delete new knowledge concept', () => {
+
+    test('should DELETE new knowledge concept', async () => {
+        await request(app)
+            .delete(conceptRoutes(CONCEPT.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .send(conceptMock.id(conceptId))
+            .expect(200)
+    })
+
+    test('should READ only one item after deleted knowledge concept', async () => {
+        const response = await request(app)
+            .get(conceptRoutes(CONCEPT.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .query(conceptMock.knowledgeId(knowledgeId))
+            .expect(200)
+
+        expect(response.body.result.data).toHaveLength(1)
+    })     
+
+})
+
+describe('Delete new user for next tests', () => {
+
+    test('should DELETE new user', async () => {
+        await request(app)
+            .delete(userRoutes(USER.DATA))
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+    })
+
+    afterAll(async () => {
+        server.close()
+        userPool.end()
+        knowledgePool.end()
+    })
+
+})
